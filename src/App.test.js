6996de94517ed53e13de1a50af07ea7ac8293ad1
Tom Harris <tom_3rd_2000@yahoo.com>
2020-10-09 00:27:35 -0400

    adding last updates, --all

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,72 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
 
-test("renders App without crashing!", () => {
+test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("Filling out the form", async () => {
+  const { getByLabelText, getByTestId, queryAllByTestId, queryByTestId } = render(<App />);
+
+  const firstNameInput = getByLabelText(/first name/i);
+
+  fireEvent.change(firstNameInput, {target: {value: "Tom"}});
+
+  expect(firstNameInput.value).toBe("Tom");
+  console.log("App.test.js: filling out the form test: firstNameInput value: ", firstNameInput.value);
+
+  await waitFor(() => {
+    fireEvent.blur(firstNameInput);
+  });
+
+  expect(queryAllByTestId("firstNameError")).toStrictEqual([]);
+
+  const lastNameInput = getByLabelText(/last name/i);
+
+  fireEvent.change(lastNameInput, {target: {value: "Harris"}});
+
+  expect(lastNameInput.value).toBe("Harris");
+  console.log("App.test.js: filling out the form test: lastNameInput value: ", lastNameInput.value);
+
+  await waitFor(() => {
+    fireEvent.blur(lastNameInput);
+  });
+
+  expect(queryAllByTestId("lastNameError")).toStrictEqual([]);
+
+  const emailInput = getByLabelText(/Email/i);
+
+  fireEvent.change(emailInput, {target: {value: "Tom_3rd_2000@yahoo.com"}});
+
+  expect(emailInput.value).toBe("Tom_3rd_2000@yahoo.com");
+  console.log("App.test.js: filling out the form test: emailInput value: ", emailInput.value);
+
+  await waitFor(() => {
+    fireEvent.blur(emailInput);
+  });
+
+  expect(queryAllByTestId("emailError")).toStrictEqual([]);
+
+  const messageInput = getByLabelText(/message/i);
+
+  fireEvent.change(messageInput, {target: {value: ""}});
+
+  expect(messageInput.value).toBe("");
+  console.log("App.test.js: filling out the form test: messageInput value: ", messageInput.value);
+
+  await waitFor(() => {
+    fireEvent.blur(messageInput);
+  })
+  
+
+  await waitFor(() => {
+    fireEvent.click(getByTestId("submitButton"));
+    
+  });
+
+  await waitFor(() => {
+    const datasubmitted = queryByTestId("datasubmitted");
+    console.log("App.test.js: filling out the form: datasubmitted: ", datasubmitted.textContent);
+  });  
 });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
@@ -7,7 +8,12 @@ const ContactForm = () => {
     mode: "onBlur",
   });
   const onSubmit = (data) => {
-    setData(data);
+    axios.post("https://reqres.in/api/contact", data)
+      .then(res => {
+        console.log("ContactForm.js: onSubmit(): axios post: res.data: ", res.data);
+        setData(res.data);
+      })
+      .catch(err => console.log(err.message));
   };
 
   return (
@@ -16,46 +22,51 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id = "firstName"
             name="firstName"
-            placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            placeholder="Tom"
+            ref={register({ required: true, minLength: 3 })}
           />
           {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
+            <p data-testid = "firstNameError">Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
 
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id = "lastName"
             name="lastName"
-            placeholder="Burke"
+            placeholder="Harris"
             ref={register({ required: true })}
           />
           {errors.lastName && (
-            <p>Looks like there was an error: {errors.lastName.type}</p>
+            <p data-testid = "lastNameError">Looks like there was an error: {errors.lastName.type}</p>
           )}
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
+          <label htmlFor="email" placeholder="Tom_3rd_2000@yahoo.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id = "email" name="email" ref={register({ required: true, pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            message: "invalid email address"
+          } })} />
           {errors.email && (
-            <p>Looks like there was an error: {errors.email.type}</p>
+            <p data-testid = "emailError">Looks like there was an error: {errors.email.message ? errors.email.message : errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea id = "message" name="message" ref={register({ required: false })} />
         </div>
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre data-testid = "datasubmitted" style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid = "submitButton" type="submit" />
       </form>
     </div>
   );
